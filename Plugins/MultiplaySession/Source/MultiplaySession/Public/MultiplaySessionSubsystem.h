ea5ef67d3c2566bb3ebc89b7137804de568d3cdc
lqy-123 <2817644793@qq.com>
2023-08-14 16:27:43 +0800

    添加子类

---
+++ Plugins/MultiplaySession/Source/MultiplaySession/Public/MultiplaySessionSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MultiplaySessionSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYSESSION_API UMultiplaySessionSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
